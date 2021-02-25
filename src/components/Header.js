612e12c98cc2ee6af5f18a6e519d098fb4347fe8
sanvean74 <dlambertpdx@gmail.com>
2021-02-24 20:47:17 -0800

    adding styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
 function Header() {
   return (
     <Container>
-      {/* <Main>
-
-        </Main> */}
-      {/* <UserContainer></UserContainer> */}
+      <Main>
+        <AccessTimeIcon />
+        <SearchContainer>
+          <Search>
+            <input type='text' placeholder='Search...' />
+          </Search>
+        </SearchContainer>
+        <HelpOutlineIcon />
+      </Main>
+      <UserContainer>
+        <Name>Donna</Name>
+        <UserImage>
+          <img src='https://i.imgur.com/6VBx3io.png' alt='user' />
+        </UserImage>
+      </UserContainer>
     </Container>
   );
 }
@@ -14,3 +28,11 @@ function Header() {
 export default Header;
 
 const Container = styled.div``;
+const Main = styled.div`
+  display: flex;
+`;
+const SearchContainer = styled.div``;
+const Search = styled.div``;
+const UserContainer = styled.div``;
+const Name = styled.div``;
+const UserImage = styled.div``;
